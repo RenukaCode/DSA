eabdb717fe9e2af6e319a018d9d7c581e7d76d5a
RenukaCode <renukadevibonam3377@gmail.com>
2025-09-04 23:30:18 +0530

    included

--- 06_Arrays/Longest_subarray[positives & negatives].py
+++ 06_Arrays/Longest_subarray[positives & negatives].py
@@ -1,20 +1,3 @@
-# Brute FOrce Approach
-# Time Complexity: O(N^3)
-# Space Complexity: O(1)
-def subarray(arr, k):
-    max_len = 0
-    for i in arr:
-        for j in range(i, len(arr)):
-            total = sum(arr[i:j + 1])
-            if total == k:
-                max_len = max(max_len, len(arr[i:j + 1]))
-    return max_len
-print(subarray([10, 5, 2, 7, 1, 9], 15))
-# Output: 4
-
-
-
-
 # Using two loops
 # Time Complexity: O(N^2)
 # Space Complexity O(1)
@@ -31,6 +14,7 @@ print(subarray([10, 5, 2, 7, 1, 9], 15))
 # Output: 4
 print(subarray([1, -1, 5, -2, 3], 3))
 # Output: 4
+print(subarray([-1, 1, 1], 1))
 
 
 
@@ -57,26 +41,4 @@ print(subarray([10, 5, 2, 7, 1, 9], 15))
 # Output: 4
 print(subarray([1, -1, 5, -2, 3], 3))
 # Output: 4
-
-
-
-# Optimal Approach using 2pointers
-# Time Complexity: O(2N)
-# Space Complexity: O(1)
-def subarray(arr, k):
-    n = len(arr)
-    left = 0
-    right = 0
-    maxlen = 0
-    sum = arr[0]
-    while right < n:
-        while left <= right and sum > k:
-            sum -= arr[left]
-            left += 1
-        if sum == k:
-            maxlen = max(maxlen, right - left + 1)
-        right += 1
-        if right < n : sum += arr[right]
-    return maxlen
-print(subarray([10, 5, 2, 7, 1, 9], 15))
-# output : 4
+print(subarray([-1, 1, 1], 1))
