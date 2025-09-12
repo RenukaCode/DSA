37837d841a496dd8e51aab7cfe686cbece110eb8
RenukaCode <renukadevibonam3377@gmail.com>
2025-09-12 22:20:36 +0530

    small insertions

--- 06_Arrays/Set_Matrix_Zero.py
+++ 06_Arrays/Set_Matrix_Zero.py
@@ -27,3 +27,33 @@ print(zeroMatrix(matrix))
           # [1, 0, 1]]
 
 
+def zero_matrix(matrix):
+    if not matrix or not matrix[0]:
+        return matrix
+    n, m = len(matrix), len(matrix[0])
+    col0 = 0
+    for i in range(n):
+        if matrix[i][0] == 0:
+            col0 = 0
+        for j in range(1, m):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+    for i in range(1, n):
+        for j in range(1, m):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+    if matrix[0][0] == 0:
+        for j in range(m):
+            matrix[0][j] = 0
+    if col0 == 0:
+        for i in range(n):
+            matrix[i][0] = 0
+    return matrix
+matrix = [[1, 1, 1],
+          [1, 0, 1],
+          [1, 1, 1]]
+print(zero_matrix(matrix))
+#  output: [[1, 0, 1],
+          # [0, 0, 0],
+          # [1, 0, 1]]           
