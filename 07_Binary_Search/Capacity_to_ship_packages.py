70b946c53985b8033113009386640e1d083c9325
RenukaCode <renukadevibonam3377@gmail.com>
2025-10-12 20:48:41 +0530

    1210

---
+++ 07_Binary_Search/Capacity_to_ship_packages.py
@@ -0,0 +1,52 @@
+def days(weights, cap):
+    n = len(weights)
+    day = 1
+    load = 0
+    for i in range(n):
+        if load + weights[i] > cap:
+            day += 1
+            load = weights[i]
+        else:
+            load += weights[i]
+    return day
+def leastCapacity(weights, d):
+    maxi = max(weights)
+    sum_ = sum(weights)
+    for i in range(maxi, sum_ + 1):
+        if days(weights, i) <= d:
+            return i
+    return -1
+print(leastCapacity([5, 4, 5, 2, 3, 4, 5, 6],5))
+# 9
+print(leastCapacity([1,2,3,4,5,6,7,8,9,10],1))
+# 55
+
+
+def days(weights, cap):
+    day = 1
+    load = 0
+    n = len(weights)
+    for i in range(n):
+        if load + weights[i] > cap:
+            day += 1
+            load = weights[i]
+        else:
+            load += weights[i]
+    return day
+def leastCapacity(weights, d):
+    low = max(weights)
+    high = sum(weights)
+    res = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if days(weights, mid) <= d:
+            res =  mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return res
+print(leastCapacity([5, 4, 5, 2, 3, 4, 5, 6],5))
+# 9
+print(leastCapacity([1,2,3,4,5,6,7,8,9,10],1))
+# 55
+
