a4c3dd89631526db4b48cddb1e7fc45ccd3f2694
RenukaCode <renukadevibonam3377@gmail.com>
2025-10-28 20:03:31 +0530

    modified

--- 07_Binary_Search/Search_in_2D_matrix.py
+++ 07_Binary_Search/Search_in_2D_matrix.py
@@ -33,6 +33,8 @@ def fn(matrix,target):
             high = mid - 1
     return False
 def search(matrix,target):
+    if isinstance(matrix[0], int):
+        return fn(matrix, target)
     n = len(matrix)
     m = len(matrix[0])
     for i in range(n):
@@ -43,6 +45,7 @@ print(search([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
 # True
 print(search([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13))
 # False
+print(search([[1,3]],3))
 
 
 
@@ -53,7 +56,7 @@ def fn(matrix, target):
     n = len(matrix)
     m = len(matrix[0])
     low = 0
-    high = n - 1
+    high = n * m - 1
     while low <= high:
         mid = (low + high) // 2
         row = mid // m
@@ -69,3 +72,4 @@ print(fn([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
 # True
 print(fn([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13))
 # False
+print(fn([[1,3]],3))
