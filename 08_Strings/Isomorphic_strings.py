6aa9bb40bfe07477f149caf61a2f68ad2492140b
RenukaCode <renukadevibonam3377@gmail.com>
2025-11-17 23:53:02 +0530

    1711 inserted

--- 08_Strings/Isomorphic_strings.py
+++ 08_Strings/Isomorphic_strings.py
@@ -13,3 +13,25 @@ print(isomorphic("egg", "add"))
 # True
 print(isomorphic("foo", "bar"))
 # False 
+
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def isomorphic(s, t):
+    s_idx = {}
+    t_idx = {}
+    for i in range(len(s)):
+        if s[i] not in s_idx:
+            s_idx[s[i]] = i
+        if t[i] not in t_idx:
+            t_idx[t[i]] = i
+        if s_idx[s[i]] != t_idx[t[i]]:
+            return False
+    return True
+print(isomorphic("paper", "title"))
+# True
+print(isomorphic("egg", "add"))
+# True
+print(isomorphic("foo", "bar"))
+# False 
