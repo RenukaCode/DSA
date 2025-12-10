000d91e73ab459b62b8633bb06f3cd14bd3a341d
RenukaCode <renukadevibonam3377@gmail.com>
2025-12-10 23:33:15 +0530

    lc stuff

---
+++ 09_LinkedList/lc-203.py
@@ -0,0 +1,38 @@
+# lc - 203. Remove Linked List Elements
+"""
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+"""      
+class Solution:
+    def removeElements(self, head, val):
+        temp = Node(0)
+        temp.next = head
+        curr = temp
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return temp.next
+"""
+    def display(self, head):
+        temp = head
+        while temp:
+            print(temp.val, end = "->")
+            temp = temp.next
+        print("None")
+"""
+# Example usage:
+"""
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(6)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(6)
+sol = Solution() 
+sol.display(head)                        # 1->2->6->3->6->None
+head = sol.removeElements(head, 6)
+sol.display(head)                        # 1->2->3->None
+"""
--- 09_LinkedList/lc-237.py
+++ 09_LinkedList/lc-237.py
@@ -1,20 +1,21 @@
-class Node:
+# lc-237. Delete Node in a Linked List
+"""class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
+        self.next = None"""
 
 class Solution:
     def deleteNode(self, node):
-        node.value = node.next.value
-        node.next = node.next.next
-    def display(self):
+        node.value = node.next.value      # 4 -> 1 -> 1 -> 9
+        node.next = node.next.next        # 4 -> 1 -> 9
+"""    def display(self):
         temp = head
         while temp:
             print(temp.value, end = " ")        
             temp = temp.next
-        print()
+        print()"""
 # Usage
-head = Node(4)  
+"""head = Node(4)  
 head.next = Node(5)
 head.next.next = Node(1)
 head.next.next.next = Node(9)
@@ -22,4 +23,4 @@ node_to_delete = head.next      # Node with value 5
 solution = Solution()
 solution.display()              # Output: 4 5 1 9
 solution.deleteNode(node_to_delete)
-solution.display()              # Output: 4 1 9   
+solution.display()              # Output: 4 1 9"""   
