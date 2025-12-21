2416c828c3c82923d67c32634f8a2a02facff8c7
RenukaCode <renukadevibonam3377@gmail.com>
2025-12-21 23:52:46 +0530

    inserted

--- 09_LinkedList/Reverse(iterative).py
+++ 09_LinkedList/Reverse(iterative).py
@@ -32,3 +32,37 @@ ll.reverse(head)
 ll.display(head)                   #4->3->2->1->None
 
 
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class LL:
+    def reverse(self, head):
+        node = None
+    def __init__(self):
+        self.head = None
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+        self.head = node
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.value, end = "->")
+            temp = temp.next
+        print("None")
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+ll = LL()
+ll.head = head
+ll.display()                          #1->2->3->4->None
+head = ll.reverse(ll.head)
+ll.display()                          #4->3->2->1->None
+
