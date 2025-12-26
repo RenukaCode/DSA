95d0177610e550465f36d18bfef520182e2053a5
RenukaCode <renukadevibonam3377@gmail.com>
2025-12-27 00:01:08 +0530

    updated

--- 09_LinkedList/Length_of_looped_list.py
+++ 09_LinkedList/Length_of_looped_list.py
@@ -29,3 +29,39 @@ head.next.next.next.next = head.next
 ll = LL()
 print(ll.lengthll(head))"""
 # 3
+
+
+# Optimal Approach
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None"""
+class LL:
+    def fn(self, head):
+        slow = fast = head
+        while fast and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return self.countLength(slow)
+    def countLength(self, point):
+        temp = point
+        length = 1
+        temp = temp.next
+        while temp != point:
+            length += 1
+            temp = temp.next
+        return length
+"""
+head = Node(3)
+head.next = Node(2)
+head.next.next = Node(0)
+head.next.next.next = Node(-4)
+head.next.next.next.next = head.next
+ll = LL()
+print(ll.fn(head))"""
+# 3
+
