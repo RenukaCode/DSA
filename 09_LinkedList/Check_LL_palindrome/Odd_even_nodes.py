982da3817832598b3c83578f7b20a6ae3fbab4ba
RenukaCode <renukadevibonam3377@gmail.com>
2025-12-30 01:58:37 +0530

    3012

---
+++ 09_LinkedList/Check_LL_palindrome/Odd_even_nodes.py
@@ -0,0 +1,54 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None"""
+class LL:
+    def seggregate(self, head):
+        if head is None or head.next is None:
+            return head
+        evenHead = evenTail = None
+        oddHead = oddTail = None
+        temp = head
+        while temp:
+            if temp.value % 2 == 0:
+                if not evenHead:
+                    evenHead = evenTail = temp
+                else:
+                    evenTail.next = temp
+                    evenTail = temp
+            else:
+                if not oddHead:
+                    oddHead = oddTail = temp
+                else:
+                    oddTail.next = temp
+                    oddTail = temp
+            temp = temp.next
+        if not evenHead:
+            return oddHead
+        evenTail.next = oddHead
+        if not oddTail:
+            return evenHead
+        oddTail.next = None
+        return evenHead
+    """
+    def display(self, head):
+        temp = head
+        while temp:
+            print(temp.value, end = "->")
+            temp = temp.next
+        print("None")"""
+"""
+head = Node(1)
+head.next = Node(4)
+head.next.next = Node(3)
+head.next.next.next = Node(2)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(6)
+ll = LL()
+ll.display(head)                         #1->4->3->2->5->6->None
+new_head = ll.seggregate(head)
+ll.display(new_head)                     #4->2->6->1->3->5->None"""
+
