391af9f55124d6fd3e1ea9dfe93816e606f305e7
RenukaCode <renukadevibonam3377@gmail.com>
2026-01-01 19:39:47 +0530

    updated

--- 09_LinkedList/Delete_nth_node.py
+++ 09_LinkedList/Delete_nth_node.py
@@ -42,3 +42,40 @@ new_head = ll.deleteNode(head, 2)
 ll.display(new_head)                     #1->2->3->5->None"""
 
 
+
+# Optimal Solution
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None"""
+class LL:
+    def deleteNode(self, head, N):
+        dummy = Node(0)
+        dummy.next = head
+        fast = slow = dummy
+        for _ in range(N+1):
+            fast = fast.next
+        while fast is not None:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
+    """
+    def display(self, head):
+        while head:
+            print(head.value, end = "->")
+            head = head.next
+        print("None")"""
+"""
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+ll = LL()
+ll.display(head)                         #1->2->3->4->5->None
+new_head = ll.deleteNode(head, 3)
+ll.display(new_head)                     #1->2->4->5->None"""
