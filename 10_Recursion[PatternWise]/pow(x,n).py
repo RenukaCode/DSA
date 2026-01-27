01303910ed545a1e7c937ebb2883c87804fcda92
RenukaCode <renukadevibonam3377@gmail.com>
2026-01-28 00:27:22 +0530

    2701

--- 10_Recursion[PatternWise]/pow(x,n).py
+++ 10_Recursion[PatternWise]/pow(x,n).py
@@ -14,3 +14,23 @@ def myPow(x,n):
     return ans
 print(myPow(2.0000, 10))               #1024.0
 print(myPow(2.0000, -2))               #0.25
+
+
+
+# Optimal Approach
+# Time Complexity: O(logn)
+# Space Complexity: O(logn)
+def myPow(x,n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+    if n % 2 == 0:
+        return myPow(x*x,n//2)
+    return x * myPow(x,n-1)
+def power(x,n):
+    if n < 0:
+        return 1.0/myPow(x,-n)
+    return myPow(x,n)
+print(power(2.0000, 10))               #1024.0
+print(power(2.0000, -2))               #0.25
