3fffa49c8a0c4de6cea1bdb771535f6699dc15e9
RenukaCode <renukadevibonam3377@gmail.com>
2026-02-03 19:45:40 +0530

    0302

---
+++ 10_Recursion[PatternWise]/generate_all_binary_strings.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(2^n)
+# Space Complexity: O(n)
+def generate(n, curr,res):
+    if len(curr)==n:
+        res.append(curr)
+        return 
+    generate(n,curr+"0",res)
+    if not curr or curr[-1] != "1":
+        generate(n,curr+"1",res)
+n = 3
+res = []
+generate(n,"",res)
+print(res)   #['000', '001', '010', '100', '101
+n = 4
+res = []
+generate(n,"",res)
+print(res)   #['0000', '0001', '0010', '010
