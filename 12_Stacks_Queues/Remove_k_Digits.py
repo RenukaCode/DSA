5971255860b5d15173ca0cd780bb41e9b369f335
RenukaCode <renukadevibonam3377@gmail.com>
2026-07-27 18:28:29 +0530

    2707

---
+++ 12_Stacks_Queues/Remove_k_Digits.py
@@ -0,0 +1,24 @@
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+def fn(nums,k):
+    st=[]
+    for num in nums:
+        while st and k>0 and st[-1]>num:
+            st.pop()
+            k-=1
+        st.append(num)
+    while st and k>0:
+        st.pop()
+        k-=1
+    if not st:
+        return  0
+    res=""
+    while st:
+        res+=st.pop()
+    res=res.rstrip('0')
+    res=res[::-1]
+    if not res:
+        return '0'
+    return res
+# print(fn("58192",2)) #192
+print(fn("1000034",2)) #3
