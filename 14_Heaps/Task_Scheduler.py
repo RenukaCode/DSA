8991474b1559211a5883b41cefa39ccc3657e66e
RenukaCode <renukadevibonam3377@gmail.com>
2026-09-09 22:33:03 +0530

    0909

---
+++ 14_Heaps/Task_Scheduler.py
@@ -0,0 +1,13 @@
+def fn(tasks,n):
+    maxFreq=0
+    maxCnt=0
+    freq={}
+    for i in tasks:
+        freq[i]=freq.get(i,0)+1
+        maxFreq=max(freq.values())
+        maxCnt=sum(1 for v in freq.values() if v==maxFreq)
+    return max((maxFreq-1)*(n+1)+maxCnt,len(tasks))
+print(fn(["A","A","A","B","B","B"],2))  # 8
+print(fn(["A","A","A","B","B","B"],0))  # 6
+print(fn(["A","C","A","B","D","B"],1))
+
